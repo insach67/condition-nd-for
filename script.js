1631dc3e018ce4a706cf6b23aa0d321ddf63ff31
Sachin Priyadarshi <97968286+insach67@users.noreply.github.com>
2022-03-03 20:54:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+// // Decision making
+// // one condition
+// let a=10
+// if(a==10){
+//     // success
+// }
+// else{
+//     // failure
+// }
+// // two condition
+// //  if else ladder
+// let a=-10;
+// if(a<0){
+//     console.log("Negative");
+// }
+// else if(a==0){
+//     console.log("Zero");
+// }
+// else{
+//     console.log("positive");
+// }
+// // three or more condition
+// // Switch case statement
+//  let gender="n"
+//  switch(gender){
+//      case "n":
+//      console.log("Male");
+//      break;
+//      case "f":
+//      console.log("female");
+//      break;
+//      default:
+//      console.log("transgender");
+//      break;
+//  }
+// // Take age as the variable age<18 child
+// // age>18 but less than 40 adult
+// // age is greater than 40old
+// let age=40;
+// if(age<18){
+//     console.log("Children");
+// }
+// else if(age>18&&age<40){
+//     console.log("Adult");
+// }
+// else{
+//     console.log("old");
+// }
+//  let age=25;
+//  switch(true){
+//      case age<18:
+//      console.log("children");
+//      break;
+//      case 18 < age <40:
+//         console.log("adult");
+//         break;
+//         default:
+//         console.log("old");
+//         break;
+//  }
+//    let day=1;
+//    switch(day)
+//    {
+//             case 0:
+//             console.log("day is monday");
+//             break; 
+//             case 1:
+//             console.log("day is tuesday");
+//             break;
+//             case 2:
+//             console.log("day is wednesday");
+//             break;
+//             case 3:
+//             console.log("day is thursady");
+//             break;
+//             case 4:
+//             console.log("day is friday");
+//             break;
+//             case 5:
+//             console.log("day is saturday");
+//             break; 
+//             default:
+//             console.log("day is sunday");
+//             break;
+        
+//    }
+//  loops
+// iteration statements
+//  for,while,do while,for each
+// []
+// initialization
+// condition
+// increment/decrement
+// var a; declaration of variable
+//  var a=12; we are initialising with a value of 12
+  
+for(let i=1;i<=10;i++){
+    result=i*19;
+    console.log("19*"+i+"="+result);
+    
+}
+
